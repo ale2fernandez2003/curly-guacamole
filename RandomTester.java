90dccfb660ad67f446c783cf75bee4f7f1522d8a
Alejandro <ale2fernandez2003@gmail.com>
2024-03-13 13:36:31 +0100

    numeros random

--- RandomTester.java
+++ RandomTester.java
@@ -1,4 +1,4 @@
-
+import java.util.Random;
 /**
  * Write a description of class RandomTester here.
  * 
@@ -7,27 +7,23 @@
  */
 public class RandomTester
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private Random random;
 
-    /**
-     * Constructor for objects of class RandomTester
-     */
-    public RandomTester()
-    {
-        // initialise instance variables
-        x = 0;
+    public RandomTester() {
+        random = new Random();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public void printOneRandom() {
+        int randomNumber = random.nextInt();
+        System.out.println(randomNumber);
+    }
+
+    public void printMultiRandom(int count) {
+        int num = 1;
+        while (num <= count) {
+            int randomNumber = random.nextInt();
+            System.out.println(randomNumber);
+            num++;
+        }
     }
 }
